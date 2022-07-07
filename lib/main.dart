a5c6061947c9772cb7d17d5d36e31b2a1bb663fe
Galadima3 <abrahamgaladima300@gmai.com>
2022-07-07 12:50:43 +0100

    Improved Neumorphic View

--- lib/main.dart
+++ lib/main.dart
@@ -26,39 +26,84 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool darkMode = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[850],
-
+      backgroundColor: darkMode ? Colors.grey[850] : Colors.grey[300],
       body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-
-          decoration: BoxDecoration(
-              color: Colors.grey[850],
-              borderRadius: const BorderRadius.all(Radius.circular(100)),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.shade900,
-                    offset: const Offset(5.0, 5.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0),
-                BoxShadow(
-                    color: Colors.grey.shade800,
-                    offset: const Offset(-5.0, -5.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0)
-              ]),
-
-          child: const Icon(
-            Icons.android,
-            size: 80,
-            color: Colors.white,
-          ), // BoxShadow 00410001( color: Colors.grey[800], offset: Offset( -5.0, -5.0), blurRadius: 15.0, spreadRadius: 1.0 ), // BoxShadow
-        ), // Boxbecoration
-      ), // Container ), // Center ): // Scaffold
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                  color: darkMode ? Colors.grey[850] : Colors.grey[300],
+                  borderRadius: const BorderRadius.all(Radius.circular(100)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: darkMode ? Colors.grey.shade900 : Colors.grey.shade300,
+                        offset: const Offset(5.0, 5.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0),
+                    BoxShadow(
+                        color: darkMode ? Colors.grey.shade800 : Colors.white,
+                        offset: const Offset(-5.0, -5.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0)
+                  ],
+                  
+                  ),
+              child: Icon(
+                Icons.android,
+                size: 80,
+                color: darkMode ? Colors.white : Colors.black,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      darkMode = true;
+                    });
+                  },
+                  child: const Text(
+                    'Dark',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ), ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      darkMode = false;
+                    });
+                  },
+                  child: const Text(
+                    'Light',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+              )
+          ],
+        ),
+      ),
     );
   }
 }
